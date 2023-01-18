067f3464bc2ef65fc0a25d6b4eab555800728632
Dmitry Bychkov <yom.4100@gmail.com>
2023-01-18 17:58:17 +0300

    add some edits

--- src/test/java/ru/netology/DeliveryFormTest.java
+++ src/test/java/ru/netology/DeliveryFormTest.java
@@ -1,10 +1,11 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exist;
@@ -13,41 +14,25 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryFormTest {
 
-    public String nearestAvailableDate() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String nearestAvailableDate = format1.format(ldt);
-
-        return nearestAvailableDate;
-    }
-
-    public String nearestDay() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd");
-        String nearestDay = format1.format(ldt);
-
-        return nearestDay;
+    public String generateDate(long days, String dataFormat) {
+        LocalDate ldt = LocalDate.now().plusDays(days);
+        DateTimeFormatter formatData = DateTimeFormatter.ofPattern(dataFormat);
+        return formatData.format(ldt);
     }
 
     @Test
     public void shouldSendForm() {
-        int i = 0;
-
-        String nearestAvailableDate = nearestAvailableDate();
-        String nearestDay = nearestDay();
-
-        String xPathForCalendar = "//td[text()='" + nearestDay + "']";
-
         Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 15000;
+
+        String nearestAvailableDate = generateDate(3,"dd.MM.yyyy");
+        String nearestDay = generateDate(3,"dd");
 
         open("http://localhost:9999");
         $x("//input[@placeholder='Город']").click();
         $x("//input[@placeholder='Город']").setValue("Казань");
         $x("//span[@data-test-id='date']").click();
-        while (i < 11) {
-            $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-            i++;
-        }
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.CONTROL+"a", Keys.BACK_SPACE));
         $x("//input[@placeholder='Дата встречи']").setValue(nearestAvailableDate);
         $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.ESCAPE);
         $x("//input[@name='name']").click();
@@ -56,30 +41,34 @@ public class DeliveryFormTest {
         $("[data-test-id='phone'] input").setValue("+79996665544");
         $("[data-test-id='agreement']").click();
         $x("//*[text()='Забронировать']").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + nearestAvailableDate))
+                .shouldBe(Condition.visible);
         $(withText("Успешно!")).waitUntil(exist, 15000);
     }
 
+
     @Test
     public void shouldSendFormWithPopups() {
-        int i = 0;
-
-        String nearestAvailableDate = nearestAvailableDate();
-        String nearestDay = nearestDay();
-
-        String xPathForCalendar = "//td[text()='" + nearestDay + "']";
-
         Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 15000;
+
+        String nextWeekDate = generateDate(14,"dd.MM.yyyy");
+        String nearestDay = generateDate(14,"d");
+
+        LocalDate current = LocalDate.now();
+        LocalDate nextWeek = LocalDate.now().plusDays(14);
+        String xPathForCalendar = "//*[text()='" + nearestDay + "']";
 
         open("http://localhost:9999");
         $x("//input[@placeholder='Город']").click();
         $x("//input[@placeholder='Город']").setValue("Ка");
         $x("//span[text()='Казань']").click();
         $x("//span[@data-test-id='date']").click();
-        while (i < 11) {
-            $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
-            i++;
+
+        if (current.getMonth() != nextWeek.getMonth()) {
+            $("[class='calendar__arrow calendar__arrow_direction_right']").click();
         }
-        $x("//input[@placeholder='Дата встречи']").setValue(nearestAvailableDate);
         $x(xPathForCalendar).click();
         $x("//input[@name='name']").click();
         $("[data-test-id='name'] input").setValue("Петров-Иванов Сидор");
@@ -87,6 +76,9 @@ public class DeliveryFormTest {
         $("[data-test-id='phone'] input").setValue("+79996665544");
         $("[data-test-id='agreement']").click();
         $x("//*[text()='Забронировать']").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + nextWeekDate))
+                .shouldBe(Condition.visible);
         $(withText("Успешно!")).waitUntil(exist, 15000);
     }
 }
